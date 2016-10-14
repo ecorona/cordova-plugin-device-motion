27edd144ecbcbac5e96336db3f28e687b518a209
Erik Corona <ecorona@live.com>
2016-10-14 13:50:47 -0500

    faltaba declarar p

--- src/android/AccelListener.java
+++ src/android/AccelListener.java
@@ -49,7 +49,7 @@ public class AccelListener extends CordovaPlugin implements SensorEventListener 
     public static int RUNNING = 2;
     public static int ERROR_FAILED_TO_START = 3;
 
-    private float x,y,z;                                // most recent acceleration values
+    private float x,y,z,p;                                // most recent acceleration values
     private long timestamp;                         // time of most recent value
     private int status;                                 // status of listener
     private int accuracy = SensorManager.SENSOR_STATUS_ACCURACY_MEDIUM;
